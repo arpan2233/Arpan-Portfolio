3ef6e18d9fc21046addc46c4ae3cad7abdc80c58
Arpan <118041697+arpan2233@users.noreply.github.com>
2024-08-29 18:18:03 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ function About(){
     return <seciton id="about-section">
         <div className="about">
             <div className="about-heading"><h3>About Me</h3></div>
-            <div className="about-content"><p>Hey Folks, Myself Arpan. I am a Computer Science Sophomore at University School of Information, Communication and Technology (Sector 16-C Dwarka, Delhi). I am passionate about my field and constantly try to know about latest technological advancements in my field. To be focused and target oriented is my topmost priority. I am the Member of All IPU DSA team , where we encourage students to learn DSA and solve there doubts. I am Open to Work . Find my skills set below and contact me if i am the one You are looking for.</p></div>
+            <div className="about-content"><p>Hi, I’m Arpan, a third-year Computer Science student at the University School of Information, Communication, and Technology (Sector 16-C Dwarka, Delhi). I’m passionate about technology and always exploring the latest advancements. With experience as a Frontend Developer at CSRBOX, I've worked on dynamic projects like the Simon Game and Clean Water and Sanitation initiatives. My skills include React.js, Node.js, Express.js, and jQuery, as well as backend technologies like PostgreSQL and SQL. I’m also skilled in HTML5, CSS, Bootstrap, and JavaScript, and I’m committed to solving problems and enhancing user experiences. As a member of the All IPU DSA team, I help students with data structures and algorithms. I’m currently open to new opportunities and eager to contribute my skills to impactful projects. Let’s connect!</p></div>
         </div>
     </seciton>
 }
