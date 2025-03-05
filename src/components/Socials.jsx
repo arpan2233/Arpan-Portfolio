3796672befbe4eb8461c6374b5a3112ac4536cc0
Arpan <118041697+arpan2233@users.noreply.github.com>
2025-03-05 22:25:15 +0530

    Update Socials.jsx

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -3,9 +3,9 @@ function Socials() {
     return <section className="social-section">
         <div className="social-heading"><h1>Socials</h1></div>
         <div className="social-svg">
-            <a href=""><img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/ig-instagram-icon.svg" /></a>
+            
             <a href=""><img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-app-icon.svg" /></a>
-            <a href=""><img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/x-social-media-logo-icon.svg" /></a>
+            <a href=""><img src="https://img.icons8.com/?size=50&id=phOKFKYpe00C&format=png" /></a>
         </div>
     </section>
 }
